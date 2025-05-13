efa46cd96b143f2f8890c9fd6ec1782695f48130
tauhid97k <tauhid.coder@gmail.com>
2025-05-13 17:50:04 +0600

    added 2fa security for super admin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,8 +2,9 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import { twoFactorClient } from "better-auth/client/plugins";
+import { redirect } from "next/navigation";
 
-export const { signIn, signUp, signOut, useSession, admin } = createAuthClient({
+export const { signIn, signUp, signOut, useSession, admin, twoFactor } = createAuthClient({
   plugins: [
     inferAdditionalFields({
       user: {
@@ -35,7 +36,11 @@ export const { signIn, signUp, signOut, useSession, admin } = createAuthClient({
         },
       },
     }),
-    twoFactorClient(),
+    twoFactorClient({
+      onTwoFactorRedirect() {
+        redirect("/2fa");
+      },
+    }),
     adminClient(),
   ],
 });
