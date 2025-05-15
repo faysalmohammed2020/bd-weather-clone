e3424ace2b67749c5ac23f706fba24493fa742e6
Jewel Rana <jewelranashepon@gmail.com>
2025-05-15 16:48:25 +0600

    Update daily model

---
+++ app/api/daily-summery/route.ts
@@ -0,0 +1,51 @@
+// app/api/daily-summary/route.ts
+
+import { getSession } from "@/lib/getSession";
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+const prisma = new PrismaClient();
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+     const session = await getSession();
+        if (!session || !session.user) {
+          return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+        }
+    const userId = session.user.id;     // Convert segmented fields (objects) to full string
+
+    
+
+    const created = await prisma.dailySummary.create({
+      data: {
+        userId,
+        dataType: body.dataType,
+        stationNo: body.stationNo,
+        month: body.month,
+        day: body.day,
+        year: body.year,
+        avStationPressure: body.avStationPressure,
+        avSeaLevelPressure: body.avSeaLevelPressure,
+        avDryBulbTemperature: body.avDryBulbTemperature,
+        avWetBulbTemperature: body.avWetBulbTemperature,
+        maxTemperature: body.maxTemperature,
+        minTemperature: body.minTemperature,
+        totalPrecipitation: body.totalPrecipitation,
+        avDewPointTemperature: body.avDewPointTemperature,
+        avRelativeHumidity: body.avRelativeHumidity,
+        windSpeed: body.windSpeed,
+        windDirectionCode: body.windDirectionCode,
+        maxWindSpeed: body.maxWindSpeed,
+        maxWindDirection: body.maxWindDirection,
+        avTotalCloud: body.avTotalCloud,
+        lowestVisibility: body.lowestVisibility,
+        totalRainDuration: body.totalRainDuration,
+      },
+    });
+
+    return NextResponse.json(created);
+  } catch (error) {
+    console.error("Failed to save daily summary:", error);
+    return new NextResponse("Server Error", { status: 500 });
+  }
+}
