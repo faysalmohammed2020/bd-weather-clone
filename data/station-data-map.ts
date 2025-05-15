b1846cd84b786b3fe259c9be1c6e79967cfd7080
BoedZisan <stack1@birdsofeden.me>
2025-05-15 15:35:38 +0600

    all json loaded

--- data/station-data-map.ts
+++ data/station-data-map.ts
@@ -1,10 +1,10 @@
 // station-data-map.ts
 
-import dhakaStation from "./dhaka_station_level";
-import dhakaSea from "./dhaka_sea_level";
+import dhakaStation from "./station_pressure_level/dhaka_station_level";
+import dhakaSea from "./station_pressure_level/dhaka_sea_level";
 
-import cxbStation from "./CXB-station_level";
-import cxbSea from "./CXB-sea_level";
+import cxbStation from "./station_pressure_level/CXB-station_level";
+import cxbSea from "./station_pressure_level/CXB-sea_level";
 
 export const stationDataMap = {
   [dhakaStation.station.station_no]: {
