abb158085f2e3380c03527dff46f12f7c64ecb86
estiak98 <estiakahmed898@gmail.com>
2025-05-18 11:08:24 +0600

    Dashboard Updated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -74,7 +74,7 @@ export default function DroughtDashboard() {
           </div>
         </div> */}
 
-        <WeatherDashboard />
+        <WeatherDashboard selectedStation={selectedStation} />
       </div>
     </div>
   );
