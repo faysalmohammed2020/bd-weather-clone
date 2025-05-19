88bd716010b39079d541e505a0bfe648afb0605c
tauhid97k <tauhid.coder@gmail.com>
2025-05-19 18:05:45 +0600

    update hour-selector

--- components/hour-selector.tsx
+++ components/hour-selector.tsx
@@ -62,12 +62,13 @@ const HourSelector = () => {
   };
 
   return (
-    <div className="bg-white p-6 border rounded-lg shadow mb-4 flex items-center gap-4">
+    <div className="bg-white p-6 border rounded-lg shadow mb-4 flex flex-col gap-4">
+      <div className="flex items-center gap-4">
       <label className="flex items-center text-lg font-bold text-amber-700">
         <Clock className="size-5 mr-2" />
         <span>UTC HOUR</span>
       </label>
-      <div className="flex flex-col space-y-4">
+      <div className="flex flex-col">
         <Select
           onValueChange={handleHourChange}
           disabled={isLoading}
@@ -118,9 +119,7 @@ const HourSelector = () => {
             </SelectItem>
           </SelectContent>
         </Select>
-        {error && (
-          <p className="text-red-500 mt-2 text-sm font-medium">{error}</p>
-        )}
+
       </div>
 
       <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
@@ -144,9 +143,9 @@ const HourSelector = () => {
           <div className="py-4">
             <div className="space-y-4">
               <div className="space-y-2">
-                <label className="text-sm font-medium">Select Hour</label>
+                <label className="text-sm font-medium mb-1 inline-block">Select Hour</label>
                 <Select onValueChange={setNewHour} value={newHour}>
-                  <SelectTrigger>
+                  <SelectTrigger className="w-full">
                     <SelectValue placeholder="Select an hour" />
                   </SelectTrigger>
                   <SelectContent>
@@ -174,6 +173,13 @@ const HourSelector = () => {
           </DialogFooter>
         </DialogContent>
       </Dialog>
+      </div>
+
+      <div className="flex items-center gap-4 bg-amber-50 border rounded-sm border-amber-300 px-4 py-2.5">
+      {error && (
+          <p className="text-red-500 text-sm font-medium">{error}</p>
+        )}
+      </div>
     </div>
   );
 };
