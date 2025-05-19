b288ebb06dff169f226e20f829879b3536c6bc9c
estiak98 <estiakahmed898@gmail.com>
2025-05-19 17:43:47 +0600

    Daily Summary Updated

--- app/dashboard/view-and-manage/daily-summery/page.tsx
+++ app/dashboard/view-and-manage/daily-summery/page.tsx
@@ -1,32 +1,12 @@
 "use client";
 
-import { useState } from "react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import WeatherDataTable from "./weatherDataTable";
 import WeatherDataForm from "./weather-data-form";
 
 export default function DailySummaryPage() {
-  const [activeTab, setActiveTab] = useState<string>("form");
 
   return (
     <main className="container mx-auto p-4 md:p-8">
-      <h1 className="text-2xl font-bold text-center mb-6">DAILY SUMMARY</h1>
-
-      <Tabs defaultValue="form" onValueChange={setActiveTab} className="w-full">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="form">Daily Summary Form</TabsTrigger>
-          <TabsTrigger value="reporttable">Report Table</TabsTrigger>
-        </TabsList>
-
-        <TabsContent value="reporttable" className="mt-6">
-          {/* <WeatherDataForm /> */}
-          <WeatherDataTable />
-        </TabsContent>
-
-        <TabsContent value="form" className="mt-6">
-          <WeatherDataForm />
-        </TabsContent>
-      </Tabs>
+      <WeatherDataForm />
     </main>
   );
 }
